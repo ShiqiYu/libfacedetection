cf87a979c5acdc31d920b08ccb03274c1c935e25
ShiqiYu <shiqi.yu@gmail.com>
2015-08-16 16:37:25 +0800

    Revert "Revert "another function added""
    This reverts commit 8e750efef4f262f21b52eafdec76fc8ff152c1bb.

--- include/facedetect-dll.h
+++ include/facedetect-dll.h
@@ -40,3 +40,9 @@ FACEDETECTDLL_API int * facedetect_multiview( unsigned char * gray_image_data, i
                                int min_neighbors, //how many neighbors each candidate rectangle should have to retain it
                                int min_size, //Minimum possible face size. Faces smaller than that are ignored.
 							   int max_size=0); //Maximum possible face size. Faces larger than that are ignored. It is the largest posible when max_size=0.
+
+FACEDETECTDLL_API int * facedetect_multiview_reinforce( unsigned char * gray_image_data, int width, int height, int step, //input image, it must be gray (single-channel) image!
+                               float scale, //scale factor for scan windows
+                               int min_neighbors, //how many neighbors each candidate rectangle should have to retain it
+                               int min_size, //Minimum possible face size. Faces smaller than that are ignored.
+							   int max_size=0); //Maximum possible face size. Faces larger than that are ignored. It is the largest posible when max_size=0.
