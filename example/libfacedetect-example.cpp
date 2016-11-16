cbd5023024a3edae62df6c97fc7f3c882cdd108a
ShiqiYu <shiqi.yu@gmail.com>
2016-11-16 22:50:03 +0800

    The bug caused by std::vector was fixed.
    The bugs in the previous version was fixed. std::vector was removed from
    the API because it can cause error.

--- example/libfacedetect-example.cpp
+++ example/libfacedetect-example.cpp
@@ -25,26 +25,39 @@ SOFTWARE.
 
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
-#include <vector>
-
 #include "facedetect-dll.h"
-//#pragma comment(lib,"libfacedetect.lib")
-#pragma comment(lib,"libfacedetect-x64.lib")
 
+#pragma comment(lib,"libfacedetect.lib")
+//#pragma comment(lib,"libfacedetect-x64.lib")
+
+//define the buffer size. Do not change the size!
+#define DETECT_BUFFER_SIZE 0xC004
 using namespace cv;
 
 int main(int argc, char* argv[])
 {
+    if(argc != 2)
+    {
+        printf("Usage: %s <image_file_name>\n", argv[0]);
+        return -1;
+    }
 	//load an image and convert it to gray (single-channel)
-	Mat gray = imread("lena.png", CV_LOAD_IMAGE_GRAYSCALE); 
+	Mat gray = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE); 
 	if(gray.empty())
 	{
-		fprintf(stderr, "Can not load the image file.\n");
+		fprintf(stderr, "Can not load the image file %s.\n", argv[1]);
 		return -1;
 	}
 
-    std::vector<FaceInfo> faces;
-	int nResult = 0; 
+	int * pResults = NULL; 
+    //pBuffer is used in the detection functions.
+    //If you call functions in multiple threads, please create one buffer for each thread!
+    unsigned char * pBuffer = (unsigned char *)malloc(DETECT_BUFFER_SIZE);
+    if(!pBuffer)
+    {
+        fprintf(stderr, "Can not alloc buffer.\n");
+        return -1;
+    }
 	
 	///////////////////////////////////////////
 	// frontal face detection 
@@ -52,50 +65,21 @@ int main(int argc, char* argv[])
 	//////////////////////////////////////////
 	//!!! The input image must be a gray one (single-channel)
 	//!!! DO NOT RELEASE pResults !!!
-	nResult = facedetect_frontal(faces, (unsigned char*)(gray.ptr(0)), gray.cols, gray.rows, (int)gray.step,
+	pResults = facedetect_frontal(pBuffer, (unsigned char*)(gray.ptr(0)), gray.cols, gray.rows, (int)gray.step,
 											   1.2f, 2,  48);
-	printf("%d frontal faces detected.\n", nResult);
+	printf("%d faces detected.\n", (pResults ? *pResults : 0));
 	//print the detection results
-	for(int i = 0; i < (int)faces.size(); i++)
+	for(int i = 0; i < (pResults ? *pResults : 0); i++)
 	{
-        FaceInfo fi = faces[i];
-        printf("face_rect=[%d, %d, %d, %d], neighbors=%d\n", fi.x, fi.y, fi.width, fi.height, fi.neighbors);
+        short * p = ((short*)(pResults+1))+6*i;
+		int x = p[0];
+		int y = p[1];
+		int w = p[2];
+		int h = p[3];
+		int neighbors = p[4];
+
+		printf("face_rect=[%d, %d, %d, %d], neighbors=%d\n", x,y,w,h,neighbors);
 	}
-
-
-	///////////////////////////////////////////
-	// multiview face detection 
-	// it can detect side view faces, but slower than facedetect_frontal().
-	//////////////////////////////////////////
-	//!!! The input image must be a gray one (single-channel)
-	//!!! DO NOT RELEASE pResults !!!
-	nResult = facedetect_multiview(faces, (unsigned char*)(gray.ptr(0)), gray.cols, gray.rows, (int)gray.step,
-											   1.2f, 5, 48);
-    printf("%d frontal faces detected.\n", nResult);
-	//print the detection results
-	for(int i = 0; i < (int)faces.size(); i++)
-	{
-        FaceInfo fi = faces[i];
-        printf("face_rect=[%d, %d, %d, %d], neighbors=%d, angle=%d\n", fi.x, fi.y, fi.width, fi.height, fi.neighbors, fi.angle);
-	}
-
-
-	///////////////////////////////////////////
-	// reinforced multiview face detection 
-	// it can detect side view faces, better but slower than facedetect_multiview().
-	//////////////////////////////////////////
-	//!!! The input image must be a gray one (single-channel)
-	//!!! DO NOT RELEASE pResults !!!
-	nResult = facedetect_multiview_reinforce(faces, (unsigned char*)(gray.ptr(0)), gray.cols, gray.rows, (int)gray.step,
-											   1.2f, 5, 48);
-    printf("%d frontal faces detected.\n", nResult);
-	//print the detection results
-	for(int i = 0; i < (int)faces.size(); i++)
-	{
-        FaceInfo fi = faces[i];
-        printf("face_rect=[%d, %d, %d, %d], neighbors=%d, angle=%d\n", fi.x, fi.y, fi.width, fi.height, fi.neighbors, fi.angle);
-	}
-
 
 	///////////////////////////////////////////
 	// frontal face detection designed for video surveillance
@@ -103,16 +87,74 @@ int main(int argc, char* argv[])
 	//////////////////////////////////////////
 	//!!! The input image must be a gray one (single-channel)
 	//!!! DO NOT RELEASE pResults !!!
-	nResult = facedetect_frontal_surveillance(faces, (unsigned char*)(gray.ptr(0)), gray.cols, gray.rows, (int)gray.step,
+	pResults = facedetect_frontal_surveillance(pBuffer, (unsigned char*)(gray.ptr(0)), gray.cols, gray.rows, (int)gray.step,
 											   1.2f, 2, 48);
-    printf("%d frontal faces detected.\n", nResult);
+	printf("%d faces detected.\n", (pResults ? *pResults : 0));
+
 	//print the detection results
-	for(int i = 0; i < (int)faces.size(); i++)
+	for(int i = 0; i < (pResults ? *pResults : 0); i++)
 	{
-        FaceInfo fi = faces[i];
-        printf("face_rect=[%d, %d, %d, %d], neighbors=%d, angle=%d\n", fi.x, fi.y, fi.width, fi.height, fi.neighbors, fi.angle);
+        short * p = ((short*)(pResults+1))+6*i;
+		int x = p[0];
+		int y = p[1];
+		int w = p[2];
+		int h = p[3];
+		int neighbors = p[4];
+
+		printf("face_rect=[%d, %d, %d, %d], neighbors=%d\n", x,y,w,h,neighbors);
 	}
 
-    return 0;
+	///////////////////////////////////////////
+	// multiview face detection 
+	// it can detect side view faces, but slower than facedetect_frontal().
+	//////////////////////////////////////////
+	//!!! The input image must be a gray one (single-channel)
+	//!!! DO NOT RELEASE pResults !!!
+	pResults = facedetect_multiview(pBuffer, (unsigned char*)(gray.ptr(0)), gray.cols, gray.rows, (int)gray.step,
+											   1.2f, 5, 48);
+	printf("%d faces detected.\n", (pResults ? *pResults : 0));
+
+	//print the detection results
+	for(int i = 0; i < (pResults ? *pResults : 0); i++)
+	{
+        short * p = ((short*)(pResults+1))+6*i;
+		int x = p[0];
+		int y = p[1];
+		int w = p[2];
+		int h = p[3];
+		int neighbors = p[4];
+		int angle = p[5];
+
+		printf("face_rect=[%d, %d, %d, %d], neighbors=%d, angle=%d\n", x,y,w,h,neighbors, angle);
+	}
+
+	///////////////////////////////////////////
+	// reinforced multiview face detection 
+	// it can detect side view faces, better but slower than facedetect_multiview().
+	//////////////////////////////////////////
+	//!!! The input image must be a gray one (single-channel)
+	//!!! DO NOT RELEASE pResults !!!
+	pResults = facedetect_multiview_reinforce(pBuffer, (unsigned char*)(gray.ptr(0)), gray.cols, gray.rows, (int)gray.step,
+											   1.2f, 5, 48);
+    printf("%d faces detected.\n", (pResults ? *pResults : 0));
+
+	//print the detection results
+	for(int i = 0; i < (pResults ? *pResults : 0); i++)
+	{
+        short * p = ((short*)(pResults+1))+6*i;
+		int x = p[0];
+		int y = p[1];
+		int w = p[2];
+		int h = p[3];
+		int neighbors = p[4];
+		int angle = p[5];
+
+		printf("face_rect=[%d, %d, %d, %d], neighbors=%d, angle=%d\n", x,y,w,h,neighbors, angle);
+	}
+
+    //release the buffer
+    free(pBuffer);
+
+	return 0;
 }
 
--- include/facedetect-dll.h
+++ include/facedetect-dll.h
@@ -36,51 +36,33 @@ SOFTWARE.
 #endif
 #endif
 
-#include <vector>
-
-#ifndef FACEDETECTDLL_EXPORTS
-typedef struct FaceInfo_{
-    int x;
-    int y;
-    int width;
-    int height;
-    int neighbors;
-    int angle;
-} FaceInfo;
-#else
-#include "object-internal.h"
-#endif
-
-//return value: the number of detected faces
-FACEDETECTDLL_API int facedetect_frontal(std::vector<FaceInfo> & faces, //(output) vector of faces
+FACEDETECTDLL_API int * facedetect_frontal(unsigned char * result_buffer, //buffer memory for storing face detection results, !!its size must be 0xC004 Bytes!!
                                unsigned char * gray_image_data, int width, int height, int step, //input image, it must be gray (single-channel) image!
                                float scale, //scale factor for scan windows
                                int min_neighbors, //how many neighbors each candidate rectangle should have to retain it
                                int min_object_width, //Minimum possible face size. Faces smaller than that are ignored.
 							   int max_object_width=0); //Maximum possible face size. Faces larger than that are ignored. It is the largest posible when max_object_width=0.
 
-//return value: the number of detected faces
-FACEDETECTDLL_API int facedetect_multiview(std::vector<FaceInfo> & faces, //(output) vector of faces
+FACEDETECTDLL_API int * facedetect_frontal_surveillance(unsigned char * result_buffer, //buffer memory for storing face detection results, !!its size must be 0xC004 Bytes!!
                                unsigned char * gray_image_data, int width, int height, int step, //input image, it must be gray (single-channel) image!
                                float scale, //scale factor for scan windows
                                int min_neighbors, //how many neighbors each candidate rectangle should have to retain it
                                int min_object_width, //Minimum possible face size. Faces smaller than that are ignored.
 							   int max_object_width=0); //Maximum possible face size. Faces larger than that are ignored. It is the largest posible when max_object_width=0.
 
-//return value: the number of detected faces
-FACEDETECTDLL_API int facedetect_multiview_reinforce(std::vector<FaceInfo> & faces, //(output) vector of faces
+FACEDETECTDLL_API int * facedetect_multiview(unsigned char * result_buffer, //buffer memory for storing face detection results, !!its size must be 0xC004 Bytes!!
                                unsigned char * gray_image_data, int width, int height, int step, //input image, it must be gray (single-channel) image!
                                float scale, //scale factor for scan windows
                                int min_neighbors, //how many neighbors each candidate rectangle should have to retain it
                                int min_object_width, //Minimum possible face size. Faces smaller than that are ignored.
 							   int max_object_width=0); //Maximum possible face size. Faces larger than that are ignored. It is the largest posible when max_object_width=0.
 
-//return value: the number of detected faces
-FACEDETECTDLL_API int facedetect_frontal_surveillance(std::vector<FaceInfo> & faces, //(output) vector of faces
+FACEDETECTDLL_API int * facedetect_multiview_reinforce(unsigned char * result_buffer, //buffer memory for storing face detection results, !!its size must be 0xC004 Bytes!!
                                unsigned char * gray_image_data, int width, int height, int step, //input image, it must be gray (single-channel) image!
                                float scale, //scale factor for scan windows
                                int min_neighbors, //how many neighbors each candidate rectangle should have to retain it
                                int min_object_width, //Minimum possible face size. Faces smaller than that are ignored.
 							   int max_object_width=0); //Maximum possible face size. Faces larger than that are ignored. It is the largest posible when max_object_width=0.
+
 
 #endif
