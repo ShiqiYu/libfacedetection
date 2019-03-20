d4ec355380470650d42aaa77f13bad0228449272
yongchao.zhang <yongchao.zhang@tendcloud.com>
2019-03-20 16:25:35 +0800

    add the iOS Demo

---
+++ mobile/iOS/FaceDetection/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FaceDetection
+//
+//  Created by Robin on 2019/3/15.
+//  Copyright © 2019 . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
